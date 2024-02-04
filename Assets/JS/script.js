bd760752b1b6c57e8dbea7abb784e6f0fdab81cc
VZachary13 <vinicius.zachary@gmail.com>
2024-02-04 12:30:52 -0700

    connected openweather data to corresponding sections and hid weather display until buttons are clicked

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,7 +1,9 @@
 
 $(function(){
 var city;
-var cityArray=[];
+var today = dayjs().format("MM/DD/YYYY");
+var todaysIcon;
+var cityList={};
     let weather = {
         apiKey: "51e27b761dce727f99c66d0530db9b8c",
         fetchWeather: function (city) {
@@ -10,6 +12,30 @@ var cityArray=[];
         },
         displayWeather: function (data) {
             console.log(data);
+
+            var counter = 0;
+            $("#five-day-display").children('section').each(function(){
+
+                do {
+                    var tempIcon = data.list[counter].weather[0].icon.substr(2, 1);
+                    console.log(tempIcon);
+                    var todaysIconTemp = todaysIcon.substr(2,1);
+                    console.log(todaysIconTemp);
+                    var tempDate = data.list[counter].dt_txt.slice('', 10);
+                    var year = tempDate.substr(0, 4);
+                    var month = tempDate.substr(5, 2);
+                    var day = tempDate.substr(8, 2);
+                    var date = month +"/"+ day + "/" + year;
+                    counter++;
+                } while ((date == today) || (tempIcon != todaysIconTemp))
+                $(this).children('.date').text(date);
+                $(this).children('img').attr('src', "http://openweathermap.org/img/w/"+data.list[counter].weather[0].icon+".png");
+                $(this).children().children('.forecast-temp').text(data.list[counter].main.temp);
+                $(this).children().children('.forecast-wind').text(data.list[counter].wind.speed);
+                $(this).children().children('.forecast-humidity').text(data.list[counter].main.humidity);
+
+                counter +=8;
+            })
         },
         fetchTodaysWeather: function (city) {
             fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+ this.apiKey +'&units=imperial').then((response) => response.json())
@@ -17,20 +43,38 @@ var cityArray=[];
         },
         displayTodaysWeather: function (data) {
             console.log(data);
+            todaysIcon = data.weather[0].icon;
             $('#city').text(data.name);
-            $('#currentIcon').attr('src', "http://openweathermap.org/img/w/"+data.weather[0].icon+".png");
-            $('#current-date').text(dayjs().format("MM/DD/YY"))
+            $('#currentIcon').attr('src', "http://openweathermap.org/img/w/"+todaysIcon+".png");
+            $('#current-date').text(today);
+            $('#tempNum').text(data.main.temp);
+            $('#wind').text(data.wind.speed);
+            $('#humidity').text(data.main.humidity);
         }
     }
 ;
 
 $('#submit').on('click', function(){
-    city = $(this).siblings('#testname').val();
+    city = $(this).siblings('#cityname').val();
     event.preventDefault();
     getCity(city);
-    saveCity(city);
-    makeButtons(city);
+    if(city){
+        saveCity(city);
+        makeButtons();
+    }
 });
+
+$('#search-history').click(function(){
+    event.preventDefault();
+    city = event.target.textContent;
+    getCity(city);
+});
+
+$('#clearHistory').click(clearSearchHistory);
+makeButtons();
+
+
+
 setInterval(function(){
     $('#time').text(dayjs().format('h:mm:ss a'));
     $("#date").text(dayjs().format('MMM DD, YYYY'));
@@ -40,33 +84,37 @@ function getCity(city) {
     if(city){
         weather.fetchTodaysWeather(city);
         weather.fetchWeather(city);
+        $("#weather").attr('style', 'display: block;');
     }else {
         alert("Please Enter A City");
     }
 }
 
 function saveCity(city) {
-    cityArray.push(city);
-    console.log(cityArray)
-    localStorage.setItem("searchHistory", JSON.stringify(cityArray));
-    cityArray = [];
+    cityList[city] = city;
+    localStorage.setItem("searchHistory", JSON.stringify(cityList));
 }
 
-function makeButtons(city) {
+function makeButtons() {
+    var searchHistory = $('#search-history');
+    cityList = JSON.parse(localStorage.getItem("searchHistory"));
+    searchHistory.html('');
+    for (var key in cityList) {
     var button = document.createElement('button');
-    cityArray.push(JSON.parse(localStorage.getItem("searchHistory")));
-    console.log(cityArray + "and"+ localStorage.getItem("searchHistory"));
-    cityArray.forEach(element => {
-        $("#search-history").append(button);
-        button.textContent = element;
-    });
+    searchHistory.append(button);
+    button.textContent = cityList[key];
+    }
+}
+
+function clearSearchHistory() {
+    var searchHistory = $('#search-history');
+    searchHistory.html('');
+    localStorage.setItem("searchHistory", '');
+    cityList = {};
 }
 
 
 
 
-// localStorage.setItem("names", JSON.stringify(names));
 
-// //...
-// var storedNames = JSON.parse(localStorage.getItem("names"));
 })//MUST BE AT BOTTOM
